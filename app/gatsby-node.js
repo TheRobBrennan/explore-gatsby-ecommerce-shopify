4abbf99aebe0ea556405f774a1ceec4f75ec755a
Rob Brennan <rob@therobbrennan.com>
2020-02-07 22:16:56 -0800

    Gave our pages query a better name

--- app/gatsby-node.js
+++ app/gatsby-node.js
@@ -2,7 +2,7 @@ const path = require("path")
 
 exports.createPages = async ({ graphql, actions: { createPage } }) => {
   const pages = await graphql(`
-    query MyQuery {
+    query MyShopifyProducts {
       allShopifyProduct {
         edges {
           node {
